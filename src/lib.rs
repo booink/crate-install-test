affbdd4d756d1c9214e48b0b00afc947dd4ea9e6
booink <booink.work@gmail.com>
2022-01-30 09:42:18 +0900

    use public crate and test.

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+pub fn test_main() {
+    println!("{}", crate_test_1::crate1_ver1::Crate1Ver1::hello());
+    println!("{}", crate_test_2::crate2_ver1::Crate2Ver1::hello());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_crate1_ver1() {
+        assert_eq!(
+            crate_test_1::crate1_ver1::Crate1Ver1::hello(),
+            "Hello version 1.0 in crate 1.".to_string()
+        );
+    }
+
+    #[test]
+    fn test_crate2_ver1() {
+        assert_eq!(
+            crate_test_2::crate2_ver1::Crate2Ver1::hello(),
+            "Hello version 1.0 in crate 2.".to_string()
+        );
+    }
+}
